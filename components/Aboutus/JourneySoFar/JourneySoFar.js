06e71e68fb0767c556f5061b73ba31eb53cb9d68
jpivista <jpivistasolutions@gmail.com>
2023-09-15 12:29:06 +0530

    journey so far changes

--- components/Aboutus/JourneySoFar/JourneySoFar.js
+++ components/Aboutus/JourneySoFar/JourneySoFar.js
@@ -19,14 +19,14 @@ const JourneySoFar = () => {
                 </Container>
             </Container>
 
-            <Container fluid style={{ background: '#F2F2F2' }}>
-                <Container className='py-4'>
-                    <Row className='w-100'>
+            <Container fluid style={{ background: '#F2F2F2' }} className='p-0 text-center'>
+                <Container className='py-md-4 p-md-0 p-2'>
+                    <Row className='w-100 p-0 mx-0'>
                         <Col md={4}>
                             <Image src='/aboutus/journeysofar/journey_left_img.png' alt='agri' fluid />
                         </Col>
-                        <Col className='text-center w-100'>
-                            <h4 className='border border-2 border-dark rounded p-5 lh-base fs-3  w-100'>Our work to empower the small scale and marginal farmers of India first began in 1994 with a simple effort to distribute indigenous seed varieties in and around the village of Thalli, Tamil Nadu. This soon turned into a growing biodiversity conservation programme that has been central to our work ever since.</h4>
+                        <Col className='text-center w-100 pt-4 pt-md-0'>
+                            <h4 className='border border-2 border-dark rounded-5 p-4 px-5 lh-base fs-3  w-100'>Our work to empower the small scale and marginal farmers of India first began in 1994 with a simple effort to distribute indigenous seed varieties in and around the village of Thalli, Tamil Nadu. This soon turned into a growing biodiversity conservation programme that has been central to our work ever since.</h4>
                             <p className='lh-base'>Side by side with farmers, a lot of research was conducted in the initial years to understand the various characteristics of indigenous seed varieties. Close to a 100 different varieties of minor millets, dry land paddy and oil seeds were tracked down and brought back to the farm at Thalli for further experimentation. Within a period of 3 years, the number of farmers we reached out to had increased from 5 to 105.</p>
                         </Col>
                     </Row>
