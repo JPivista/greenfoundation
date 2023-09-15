8c0f119fd3d932e8774740c408318298d564a7f8
jpivista <jpivistasolutions@gmail.com>
2023-09-15 12:14:44 +0530

    our work and news and updates changes

--- components/Home/OurWork.js
+++ components/Home/OurWork.js
@@ -4,24 +4,24 @@ import { Col, Container, Row } from 'react-bootstrap'
 const OurWork = () => {
     return (
         <>
-            <Container className='py-5 p-0 w-100 p-2'>
-                <h1 className='text-dark text-center'>Our Work</h1>
-                <Row className='d-flex flex-lg-row flex-column'>
-                    <Col className='agro-bg-home m-2 p-4 d-flex flex-column justify-content-center' md>
+            <Container className='py-5 p-md-0 w-100 p-3'>
+                <h1 className='text-dark text-center py-3'>Our Work</h1>
+                <Row className='d-flex flex-lg-row flex-column  p-md-0 gap-3'>
+                    <Col className='agro-bg-home  p-4 d-flex flex-column justify-content-center' md>
                         <h2 className='text-white'>Agrobiodiversity Conservation</h2>
                         <p className='text-white mb-0'>Conservation of indigenous seed varieties has been at the core of GREEN Foundation’s work since 1994…</p>
                     </Col>
-                    <Col className='cer-bg-home m-2 p-4 d-flex flex-column justify-content-center'>
+                    <Col className='cer-bg-home p-4 d-flex flex-column justify-content-center'>
                         <h2 className='text-white'>Certification</h2>
                         <p className='text-white mb-0'>A collaborative approach which involves farmers and other stakeholders in verifying the authenticity of organic produce…</p>
                     </Col>
                 </Row>
-                <Row className='d-flex flex-lg-row flex-column'>
-                    <Col className='sustain-bg-home m-2 p-4 d-flex flex-column justify-content-center' >
+                <Row className='d-flex flex-lg-row flex-column p-md-0 gap-3 mt-md-3'>
+                    <Col className='sustain-bg-home p-4 d-flex flex-column justify-content-center' >
                         <h2 className='text-white'>Sustainable Agriculture</h2>
                         <p className='text-white mb-0'>Sustainable agriculture is the production of plant and animal products in a way that uses farming techniques to protect the environment…</p>
                     </Col>
-                    <Col className='research-bg-home m-2 p-4 d-flex flex-column justify-content-center'>
+                    <Col className='research-bg-home p-4 d-flex flex-column justify-content-center'>
                         <h2 className='text-white'>Research and Communication</h2>
                         <p className='text-white mb-0'>Research enables us to achieve our goals in Sustainable Agriculture and Agro-biodiversity conservation.</p>
                     </Col>
