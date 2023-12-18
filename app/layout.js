b54557a2b199ed1cd882012414d015e3f1c912b3
JPivista <jpivistasolutions@gmail.com>
2023-12-18 15:55:40 +0530

    favicon GTM Code updated

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </Head>
       {/* Start Google Analytics Manager */}
       <Script
         strategy="afterInteractive"
@@ -42,6 +49,7 @@ export default function RootLayout({ children }) {
           })(window,document,'script','dataLayer','GTM-5J2C4PF'');
         `}
       </Script>
+
 
       <body>
 
